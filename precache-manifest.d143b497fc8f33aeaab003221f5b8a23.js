258102b38d615901b31be6f02914eb583077bd11
Dyn4sty <liorkasha17@gmail.com>
2019-11-15 14:57:54 +0200

    Updates

---
+++ precache-manifest.d143b497fc8f33aeaab003221f5b8a23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bd8b33b1eb6b2d4e774faa30c8f2b0d",
+    "url": "/brainiac/index.html"
+  },
+  {
+    "revision": "34c154f5865e6b3f1ad4",
+    "url": "/brainiac/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "2c42c3b9ac374af22b19",
+    "url": "/brainiac/static/css/main.caa06e46.chunk.css"
+  },
+  {
+    "revision": "34c154f5865e6b3f1ad4",
+    "url": "/brainiac/static/js/2.8b4c4fbc.chunk.js"
+  },
+  {
+    "revision": "2c42c3b9ac374af22b19",
+    "url": "/brainiac/static/js/main.ea8b2c64.chunk.js"
+  },
+  {
+    "revision": "db20eb46eb271a74adfd",
+    "url": "/brainiac/static/js/runtime-main.940b53cb.js"
+  },
+  {
+    "revision": "6afe8ffa6203d2f9b77b510f2d7ae6b4",
+    "url": "/brainiac/static/media/brain.6afe8ffa.png"
+  }
+]);
