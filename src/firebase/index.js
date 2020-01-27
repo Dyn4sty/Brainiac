a04ebf79fb71c6d64af43ccab052b2f79e01c87a
Dyn4sty <liorkasha17@gmail.com>
2020-01-27 14:54:06 +0200

    Dynamically incrementing the entries.

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,14 +3,14 @@ import "firebase/storage";
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyD2_Ww5mf2mEskOQMa6IYVK6URF9GqMbUE",
-  authDomain: "quickstart-1571266618401.firebaseapp.com",
-  databaseURL: "https://quickstart-1571266618401.firebaseio.com",
-  projectId: "quickstart-1571266618401",
-  storageBucket: "quickstart-1571266618401.appspot.com",
-  messagingSenderId: "496997080126",
-  appId: "1:496997080126:web:8caa4fb9ce5e4204df456f",
-  measurementId: "G-72E4NBCF0G"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 firebase.initializeApp(config);
 
